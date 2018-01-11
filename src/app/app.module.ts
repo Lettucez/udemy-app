f12590289c9e736300a2c7a4ba490afb7c3f1bc8
cleo <cleo@wvuf.org>
2018-01-11 14:23:23 -0500

    create skeleton app with navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeListItemComponent } from './recipes/recipe-list-item/recipe-list-item.component';
+import { RecipeInformationComponent } from './recipes/recipe-information/recipe-information.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeListItemComponent,
+    RecipeInformationComponent,
+    RecipesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recipes/recipe-information/recipe-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-information',
+  templateUrl: './recipe-information.component.html',
+  styleUrls: ['./recipe-information.component.css']
+})
+export class RecipeInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
